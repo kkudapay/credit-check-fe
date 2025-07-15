332993b52a1fa6346eb8571aec656fc1ffc68314
Ra Seokhee <raseokhe1@gmail.com>
2025-07-15 11:50:59 +0900

    feat52/CSP 적용

--- next.config.js
+++ next.config.js
@@ -1,10 +1,54 @@
 /** @type {import('next').NextConfig} */
+
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' https://www.googletagmanager.com https://www.google-analytics.com 'unsafe-inline';
+  style-src 'self' 'unsafe-inline' https://fonts.googleapis.com;
+  img-src 'self' data: https://www.google-analytics.com https://www.googletagmanager.com;
+  font-src 'self' https://fonts.gstatic.com;
+  connect-src 'self' https://amexdwsazbwkgtbupovh.supabase.co https://www.google-analytics.com;
+  frame-src https://www.googletagmanager.com;
+`;
+
+const securityHeaders = [
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+  },
+  {
+    key: 'Referrer-Policy',
+    value: 'strict-origin-when-cross-origin',
+  },
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+];
+
 const nextConfig = {
   //output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
